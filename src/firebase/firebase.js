684391e4ea2e555ea38b01ff2d85cff0f5cbad0c
Fernando Harianto <fernandoharianto16@gmail.com>
2024-11-09 02:35:58 +0700

    tes gitignore
    testing

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,35 +1,3 @@
-// // src/firebase/firebase.js
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-
-// const firebaseConfig = {
-//   apiKey: "YOUR_API_KEY",
-//   authDomain: "YOUR_AUTH_DOMAIN",
-//   projectId: "YOUR_PROJECT_ID",
-//   storageBucket: "YOUR_STORAGE_BUCKET",
-//   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-//   appId: "YOUR_APP_ID"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const auth = getAuth(app);
-
-// export { auth };
-
-// import { initializeApp } from 'firebase/app';
-
-// // TODO: Replace the following with your app's Firebase project configuration
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCYpRVUrQpHb9m5FpD5XDnuhPKxKwVfaLw",
-//     authDomain: "YOUR_AUTH_DOMAIN",
-//     projectId: "YOUR_PROJECT_ID",
-//     storageBucket: "YOUR_STORAGE_BUCKET",
-//     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-//     appId: "YOUR_APP_ID"
-// };
-
-// const app = initializeApp(firebaseConfig);
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
